f41382f5ca34e52c0217eada8821afa9481a4415
ph3mt <ph3mt@users.noreply.github.com>
2024-12-23 16:53:20 +0000

    deploy: 7a26422a8cd45fd115560739bc74a8745fe51cf5

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "GodFather Android Malware Analysis __",
+        "title": "GodFather Android Malware Analysis",
         "excerpt":"GodFather is an Android malware that was first identified in mid 2023 and quickly attracted the attention of security experts because of its advanced capabilities and modular structure. Its design highlights a significant evolution from its predecessors, exploiting sophisticated techniques to circumvent security measures and infect Android devices. In this...","categories": ["Blog"],
         "tags": ["Malware","Android"],
         "url": "/blog/2024/12/10/godfather-android-analysis.html",
